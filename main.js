a009cc6ff66ba594ada60f99c42f89d2e3aebadd
Pontes <hpo.pontes@gmail.com>
2023-05-18 14:16:30 -0300

    editando e atualizando a string no localstorage

--- main.js
+++ main.js
@@ -6,16 +6,26 @@ form.addEventListener('submit', (evento)=>{
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento => elemento.nome === nome.value)
     const itemAtual = {
         "nome": nome.value, 
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
-    itens.push(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+        console.log(existe.id)
 
+        atualizaElemento(itemAtual)
+        itens[existe.id] = itemAtual
+    }else{
+        itemAtual.id = itens.length
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+    }
+    
     localStorage.setItem("itens", JSON.stringify(itens))//metodo stringfy json só guarda string no
-
+    //localStorage.clear(itens) para apagar o localStorage
     nome.value = ""
     quantidade.value = ""
 })
@@ -34,10 +44,12 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
-
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
     lista.appendChild(novoItem)
+}
 
-    
+function atualizaElemento(item){
+    document.querySelector("[data-id= '"+item.id+"']").innerHTML = item.quantidade
 }
